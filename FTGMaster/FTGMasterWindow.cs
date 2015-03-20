cb8bc01a69c504f51225edbafe2b44916a79218b
OpenFibers <openfibers@gmail.com>
2015-03-20 20:35:34 +0800

    added pressed key and time table

--- FTGMaster/FTGMasterWindow.cs
+++ FTGMaster/FTGMasterWindow.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Kennedy.ManagedHooks;
 using FTGMaster.MacroProfiles;
+using DirectXKeyParserNameSpace;
 
 //TODO：补齐脚本注释
 //TODO：按键记录，用于判断后面的触发条件
@@ -21,18 +22,23 @@ namespace FTGMaster
     {
         private IKeyboardHookExt _keyboardHook = null;
         private MacroProfile _currentProfile = null;
+        private Dictionary<String, int> _keyPressedTimeDictionary = null;
 
         public FTGMasterWindow()//构造函数
         {
             InitializeComponent();
 
-            //安装钩子，回调在KeyDownEventCallback
+            //安装钩子，回调在KeyEventCallback
             _keyboardHook = HookFactory.CreateKeyboardHookExt();
             _keyboardHook.KeyDown += new KeyboardEventHandlerExt(KeyDownEventCallback);
+            _keyboardHook.KeyUp += new KeyboardEventHandlerExt(KeyUpEventCallback);
             _keyboardHook.InstallHook();
 
             //读取当前的profile文件
             _currentProfile = MacroProfile.ProfileFromFileRelativePath("default.txt");
+
+            //初始化
+            _keyPressedTimeDictionary = new Dictionary<String, int>();
         }
 
         ~FTGMasterWindow()
@@ -46,9 +52,31 @@ namespace FTGMaster
 
         private void KeyDownEventCallback(object sender, KeyboardHookEventArgs kea)//按键回调
         {
+            this.KeyEventCallback(sender, kea, SingleMacroActionType.Press);
+        }
+
+        private void KeyUpEventCallback(object sender, KeyboardHookEventArgs kea)//按键回调
+        {
+            this.KeyEventCallback(sender, kea, SingleMacroActionType.Lift);
+        }
+
+        private void KeyEventCallback(object sender, KeyboardHookEventArgs kea, SingleMacroActionType type)//按键回调
+        {
             kea.Cancel = true;
+
+
+            int currentTime = 0;
+            if (type == SingleMacroActionType.Press)
+            {
+                String keyString = kea.Key.ToString();
+                DirectXKeyCode code = DirectXKeyParser.DirectXKeyScanCodeFromString(keyString);
+                if (code != DirectXKeyCode.None)
+                {
+                    _keyPressedTimeDictionary.Add(keyString, currentTime);
+                }
+            }
             string s = Keys.A.ToString();
-            string msg = string.Format("\nKeyDown event: {0}.", kea.ToString());
+            string msg = string.Format("\nKeyDown event: {0}.", sender.ToString());
 
             //
             // Prevent the 'A' key from reaching any applications whatsoever
@@ -66,7 +94,7 @@ namespace FTGMaster
         private void aaa()
         {
             Console.WriteLine("\\");
-            int scanCode = (int)DirectXHelper.DirectXKeyParser.DirectXKeyScanCodeFromString("Escape");
+            int scanCode = (int)DirectXKeyParser.DirectXKeyScanCodeFromString("Escape");
             SendInputHelper.SendInputHelper.DirectInputKeyDown(38);
         }
     }
