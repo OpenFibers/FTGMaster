042ecc67242517dd2ac06dac7983a89b6b781e9f
OpenFibers <openfibers@gmail.com>
2015-03-22 04:37:49 +0800

    SingleMacroTriggerModifier added

---
+++ FTGMaster/MacroProfiles/SingleMacroTriggerModifier.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTGMaster.MacroProfiles
+{
+    class SingleMacroTriggerModifier
+    {
+        private List<String> _modifierKeyStrings;
+
+        private SingleMacroTriggerModifier()
+        {
+            _modifierKeyStrings = new List<String>();
+        }
+
+        private void AddKeyString(String keyString)
+        {
+            _modifierKeyStrings.Add(keyString);
+        }
+
+        public static SingleMacroTriggerModifier SingleMacroTriggerModifierFromOptionString(String optionString)
+        {
+            if (optionString == null)
+            {
+                return null;
+            }
+            if (optionString.Length == 0)
+            {
+                return null;
+            }
+
+            SingleMacroTriggerModifier modifier = new SingleMacroTriggerModifier();
+            String[] singleOptionStrings = optionString.Split('&');
+            foreach (String singleOptionString in singleOptionStrings)
+            {
+                if (singleOptionString != null && 
+                    singleOptionString.Length != 0)
+                {
+                    modifier.AddKeyString(singleOptionString);
+                }
+            }
+            return modifier;
+        }
+    }
+}
