d23dd5466e5af5ba7a2805886ac224066bdbcfad
OpenFibers <openfibers@gmail.com>
2015-03-21 23:49:19 +0800

    macro excution queue added

---
+++ FTGMaster/MacroProfiles/SingleMacroExcutionQueue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTGMaster.MacroProfiles
+{
+    class SingleMacroExcutionQueue
+    {
+        public SingleMacroExcutionQueue(SingleMacro macro, int delayMilliseconds)
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+}
